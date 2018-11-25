f3bccd7907ddbad6f52d7696bf2b3ff87d27784c
Ludivine <ludivine.pellissier@gmail.com>
2018-11-25 04:32:55 +0100

    add download file

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  res.header('Access-Control-Allow-Headers', '*')
   next()
 })
 
